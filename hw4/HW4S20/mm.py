a10545fb1c3f7e2e54cfe1908fe160cd4e26a7c1
Cam Gallucci <cgallucci99@gmail.com>
2020-03-31 21:31:11 -0400

    finish python mm

---
+++ hw4/HW4S20/mm.py
@@ -0,0 +1,43 @@
+import sys
+
+#open files from command line args
+data = open(sys.argv[1])
+template = open(sys.argv[2])
+
+#put lines of data file into a list
+lines = data.readlines()
+
+#split the first line at the commas and subtract the last newline char
+headers = lines[0][0:len(lines[0])-1].split(',')
+
+#extract template from file
+temp = template.read()
+
+#make dictionary of all columns
+cols = {}
+for i in range(0, len(headers)):
+    cols[headers[i]] = i
+
+# #find ID column
+# id = -1
+# for i in range(0, len(headers)):
+#     if headers[i] == "ID":
+#         id = i
+#         break
+
+#print(cols)
+
+#loop through each line of CSV file
+for i in range(1, len(lines)):
+    line = lines[i][0:len(lines[i])-1].split(',')
+    #print(line)
+    filename = line[cols["ID"]] + ".txt"
+    userFile = open(filename, "w")
+    result = temp
+    for col in cols.keys():
+        #print(result)
+        index = result.find("<<" + col + ">>")
+        length = len("<<" + col + ">>")
+        result = result[0:index] + line[cols[col]] + result[index+length:]
+    print(result)
+    userFile.write(result)
